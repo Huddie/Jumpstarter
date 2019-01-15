88c2711f923b549f4670dc90772bd26a8dcf46f8
Ehud Adler <easports96@gmail.com>
2019-01-14 22:32:16 -0500

    Fixed inst error

--- lib/jumpstarter_core/instructions.rb
+++ lib/jumpstarter_core/instructions.rb
@@ -362,7 +362,7 @@ module Jumpstarter
 
         def run!()
             Dir.chdir "#{@proj_path}"
-            scheme_file_path = Dir.glob("./**/#{@scheme_name}".xcscheme)
+            scheme_file_path = Dir.glob("./**/#{@scheme_name}.xcscheme")
             dir_of_scheme = File.dirname(scheme_file_path)
             File.rename(scheme_file_path, dir_of_scheme + File::SEPARATOR + "Copy of " + scheme_file_path)
             return true
